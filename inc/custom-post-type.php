07c748c76dd1c2a125aa6b79ddc2aa699eed7538
Xiradorn <xiradorn@gmail.com>
2018-08-24 17:28:47 +0200

    Part12 Custom Metabox save data and retrive

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -18,6 +18,12 @@ if ( @$options == 1 ) {
 	// now we can customize the editing property of the single column
 	add_action( 'manage_gattoverde-contact_posts_custom_column', 'gattoverde_set_gattoverde_custom_column', 10, 2 );
 
+	// add anb action for generate custom metabox
+	add_action( 'add_meta_boxes', 'gattoverde_contact_add_meta_box' );
+
+	//add action for save post with new meta infos
+	add_action( 'save_post', 'gattoverde_save_contact_email_data' );
+
 }
 
 /** Contact CPT */
@@ -71,7 +77,8 @@ function gattoverde_set_gattoverde_custom_column( $column, $post_id ) {
 		break;
 
 		case 'email' :
-			//email column
+			$email = get_post_meta( $post_id, '_gv_contact_email_value_key', true );
+			echo '<a href="mailto:' . $email . '">' . $email . '</a>';
 		break;
 		
 		default:
@@ -79,5 +86,53 @@ function gattoverde_set_gattoverde_custom_column( $column, $post_id ) {
 		break;
 	
 	}
+
+}
+
+// generation of custom meta box area for collects a couple of info
+function gattoverde_contact_add_meta_box() {
+
+	// add_meta_box( $id, $title, $callback, $screen = null, $context = 'advanced', $priority = 'default', $callback_args = null )
+	add_meta_box( 'contact_email', 'User Email', 'gattoverde_contact_email_callback', 'gattoverde-contact', 'side', 'default' );
+
+}
+
+// we need to insert a vars $post for grab all info linked to post
+function gattoverde_contact_email_callback( $post ) {
+
+	wp_nonce_field( 'gattoverde_save_contact_email_data', 'gattoverde_contact_email_meta_box_nonce' );
+
+	$value = get_post_meta( $post->ID, '_gv_contact_email_value_key', true );
+
+	echo '<label for="gattoverde_contact_email_field">User Email Address: </label>';
+	echo '<input type="email" id="gattoverde_contact_email_field" name="gattoverde_contact_email_field" value="' . esc_attr( $value ) . '" size="25" />';
+
+}
+
+function gattoverde_save_contact_email_data( $post_id ) {
+
+	if( ! isset( $_POST['gattoverde_contact_email_meta_box_nonce'] ) ) {
+		return;
+	}
+
+	if( ! wp_verify_nonce( $_POST['gattoverde_contact_email_meta_box_nonce'], 'gattoverde_save_contact_email_data' ) ) {
+		return;
+	}
+
+	if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+
+	if( ! isset( $_POST['gattoverde_contact_email_field'] ) ) {
+		return;
+	}
+
+	$my_data = sanitize_text_field( $_POST['gattoverde_contact_email_field'] );
+
+	update_post_meta( $post_id, '_gv_contact_email_value_key', $my_data );
 
 }
