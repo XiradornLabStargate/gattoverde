69a9587f26fedd56f0c69e9606c1d4c1e2d16e8f
Xiradorn <xiradorn@gmail.com>
2018-08-29 01:09:27 +0200

    SPECIAL Gallery slider fix in JS for multiple carousels

--- js/gattoverde.js
+++ js/gattoverde.js
@@ -26,11 +26,15 @@ jQuery( document ).ready( function( $ ) {
 
 	function gattoverde_get_bs_thumbs( carousel ) {
 
-		var nextThumb = $( '.carousel-item.active' ).find( '.next-image-preview' ).data( 'image' );
-		var prevThumb = $( '.carousel-item.active' ).find( '.prev-image-preview' ).data( 'image' );
-		// console.log(nextThumb);
-		$( carousel ).find( '.carousel-control-next' ).find( '.thumbnail-container' ).css( { 'background-image' : 'url( '+nextThumb+' )' } );
-		$( carousel ).find( '.carousel-control-prev' ).find( '.thumbnail-container' ).css( { 'background-image' : 'url( '+prevThumb+' )' } );
+		$( carousel ).each( function( index, el ) {
+			
+			var nextThumb = $( this ).find( '.carousel-item.active' ).find( '.next-image-preview' ).data( 'image' );
+			var prevThumb = $( this ).find( '.carousel-item.active' ).find( '.prev-image-preview' ).data( 'image' );
+			// console.log(nextThumb);
+			$( this ).find( '.carousel-control-next' ).find( '.thumbnail-container' ).css( { 'background-image' : 'url( '+nextThumb+' )' } );
+			$( this ).find( '.carousel-control-prev' ).find( '.thumbnail-container' ).css( { 'background-image' : 'url( '+prevThumb+' )' } );
+
+		} );
 	
 	}
 
